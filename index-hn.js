3be4096788fd3d3ea45396e48ccd3a5131a7aacd
aookapp <88180405+aookapp@users.noreply.github.com>
2026-03-12 22:08:55 +0800

    Update index-hn.js

--- index-hn.js
+++ index-hn.js
@@ -7,8 +7,8 @@ const TASKS = [
   { url: "https://itv.aptv.app/china-iptv/hnyd.m3u", ua: "AptvPlayer/1.2.5(iPhone)" },
   { url: "https://itv.aptv.app/china-iptv/zgyd.m3u", ua: "AptvPlayer/1.2.5(iPhone)" },
   { url: "https://itv.5iclub.dpdns.org/MiGu.m3u", ua: "AptvPlayer/1.2.5(iPhone)" },
-  { url: "https://raw.githubusercontent.com/aookapp/kankan/main/www.m3u", ua: "Mozilla/5.0" },
-  { url: "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u", ua: "Mozilla/5.0" }
+  { url: "https://raw.githubusercontent.com/aookapp/kankan/main/www.m3u", ua: "Mozilla/5.0" }
+//   { url: "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u", ua: "Mozilla/5.0" }
 ];
 
 // --- 2. 填写合并后的 EPG 链接 ---
