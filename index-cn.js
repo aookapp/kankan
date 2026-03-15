01bd4b4132cad9012e2f05a4a6dba77611e418c9
aookapp <88180405+aookapp@users.noreply.github.com>
2026-03-15 14:07:24 +0800

    Update index-cn.js

--- index-cn.js
+++ index-cn.js
@@ -168,6 +168,7 @@ async function main() {
   }
 
   // 写入文件
+  fs.writeFileSync('kankan-cn.m3u', output);
   fs.writeFileSync('cn.m3u', output);
   console.log(`\n🎉 处理完成！`);
   console.log(`收集到了 ${globalEpgUrls.size} 个 EPG 节目单链接。`);
